8faf6a4ccd19a2db046b6624ac6cfe9380f919d0
DanNBullock <dnbulloc@indiana.edu>
2018-05-16 15:10:07 -0400

    Change of merge and use of case statements

--- bsc_mergeClassifications.m
+++ bsc_mergeClassifications.m
@@ -22,13 +22,16 @@ uniqueNamesLength=length(uniqueNamesTotal);
 if uniqueNamesLength==presumeNameNum
     %hyper inelegant, guarenteed to cause problems.
     classification.names=horzcat(baseNames,addNames);
-    addIndex=[classificationAdd.index]+[classificationAdd.index>0] *baseNameNum;
-    classification.index(classificationAdd.index>0)=0;
-    if sum(size(addIndex)==size(baseClassification.index))==2
-    classification.index=baseClassification.index+addIndex;
-    else
-        classification.index=baseClassification.index+addIndex';
-    end
+    classificationAdd.index(classificationAdd.index>0)=classificationAdd.index(classificationAdd.index>0)+baseNameNum;
+    classification.index=horzcat(baseClassification.index,classificationAdd.index);
+  
+%     addIndex=[classificationAdd.index]+[classificationAdd.index>0] *baseNameNum;
+%     classification.index(classificationAdd.index>0)=0;
+%     if sum(size(addIndex,1)==size(baseClassification.index,1))==2
+%     classification.index=horzcat(baseClassification.index+addIndex;
+%     else
+%         classification.index=baseClassification.index+addIndex';
+%     end
 else
     keyboard
     %WRITE THIS
--- bsc_mergeFGandClass.m
+++ bsc_mergeFGandClass.m
@@ -3,44 +3,50 @@ function [mergedFG, mergedClassification]=bsc_mergeFGandClass(inputFGs,inputClas
 %
 %  Requires vistasoft
 
-if length(inputFGs)~=length(inputClassifications)
-    error('Mismatch between length of input fiber groups and classification structures')
+if ~exist('inputClassifications') ==1
+    inputClassifications=[];
 end
 
-
-
 for iInputs=1:length(inputFGs)
-    if iInputs==1
-        if iscell(inputFGs)
-            [mergedFG, ~] = bsc_LoadAndParseFiberStructure(inputFGs{iInputs});
-        end
-        if iscell(inputClassifications)
-            if or(ischar(inputClassifications{iInputs}),inputClassifications(inputFGs{iInputs}))
-                mergedClassification=load(inputClassifications{iInputs});
-            elseif isstruc(ischar(inputClassifications{iInputs}))
-                mergedClassification=inputClassifications{iInputs};
-            else
-                warning('\n Input classification type not recognized for input %i',iInputs)
-            end
-        end
-    end
-    if iInputs>=2
-        if iscell(inputFGs)
-            [toMergeFG, ~] = bsc_LoadAndParseFiberStructure(inputFGs{iInputs});
-        end
-        if iscell(inputClassifications)
-            if or(ischar(inputClassifications{iInputs}),inputClassifications(inputFGs{iInputs}))
-                toMergeclassification=load(inputClassifications{iInputs});
-            elseif isstruc(ischar(inputClassifications{iInputs}))
-                toMergeclassification=inputClassifications{iInputs};
-            else
-                warning('\n Input classification type not recognized for input %i',iInputs)
-            end
+    
+    [toMergeFG, ~] = bsc_LoadAndParseFiberStructure(inputFGs{iInputs});
+    
+    if ~or(length(toMergeFG.fibers)==0,length(toMergeFG.fibers{1})==0);
+        if ~exist('mergedFG') ==1
+            mergedFG=toMergeFG;
+            mergedClassification.names=[];
+            mergedClassification.index=[];
+        else
+            mergedFG=fgMerge(mergedFG,toMergeFG)
         end
         
-        
-        mergedFG=fgMerge(mergedFG,toMergeFG);
-        mergedClassification = bsc_mergeClassifications(mergedClassification,toMergeclassification);       
+        switch length(inputClassifications)
+            case 0
+                toMergeclassification.names{1}=toMergeFG.name;
+                toMergeclassification.index(1:length(toMergeFG.fibers))=1;
+            case 1
+                if length(mergedClassification.names)==0
+                    if or(ischar(inputClassifications{iInputs}),inputClassifications(inputFGs{iInputs}))
+                        mergedClassification=load(inputClassifications{iInputs});
+                    elseif isstruc(ischar(inputClassifications{iInputs}))
+                        mergedClassification=inputClassifications{iInputs};
+                    else
+                        warning('\n Input classification type not recognized for input %i',iInputs)
+                    end
+                else
+                    toMergeclassification.names{1}=toMergeFG.name;
+                    toMergeclassification.index(1:length(toMergeFG.fibers))=1;
+                end
+            case length(inputFGs)
+                 if or(ischar(inputClassifications{iInputs}),inputClassifications(inputFGs{iInputs}))
+                    toMergeclassification=load(inputClassifications{iInputs});
+                elseif isstruc(ischar(inputClassifications{iInputs}))
+                    toMergeclassification=inputClassifications{iInputs};
+                else
+                    warning('\n Input classification type not recognized for input %i',iInputs)
+                 end
+        end
+         mergedClassification = bsc_mergeClassifications(mergedClassification,toMergeclassification);
     end
 end
 
